23d044d20829404a1d31101312df3d57ae13f2b2
lctking <1713673102lw@gmail.com>
2025-01-15 23:31:26 +0800

    添加合适的注释

--- src/main/java/com/lctking/shardingtest/algorithm/UserDataBaseComplexAlgorithm.java
+++ src/main/java/com/lctking/shardingtest/algorithm/UserDataBaseComplexAlgorithm.java
@@ -31,7 +31,7 @@ public class UserDataBaseComplexAlgorithm implements ComplexKeysShardingAlgorith
     private static final String SHARDING_TABLE_NODES_SUFFIX = "-sharding-table-nodes";
 
     /**
-     * 分表总数-查询键
+     * 单个数据库下分表数-查询键
      */
     private static final String TABLE_SHARDING_COUNT = "table-sharding-count";
     @Override
@@ -47,21 +47,28 @@ public class UserDataBaseComplexAlgorithm implements ComplexKeysShardingAlgorith
             Comparable<?> regionCompare = columnNameAndShardingValuesMap.get(regionStr).stream().findFirst().get();
             String dbSuffix;
             String actualRegion = regionCompare.toString();
-
+            //根据region地区，取到分表节点信息（形如{0..3}，表示当前地区有t_user_0到3。）
+            //tableShardingNodes中存放的是[0,3]
             List<Integer> tableShardingNodes = getTableShardingNodes(actualRegion.toLowerCase());
 
             if(CollUtil.isEmpty(tableShardingNodes)){
-                //如果根据当前region找不到数据表，则选择放入other地区的数据表中
-                //databaseShardingNodes = getDatabaseShardingNodes("other");
+                //如果根据当前region找不到数据表，则选择查询other地区的数据表（也意味着准备放入other地区对应的数据库表中
                 tableShardingNodes = getTableShardingNodes("other");
             }
             if(CollUtil.isNotEmpty(tableShardingNodes)){
+                //取到分表节点信息（形如{0..3}，表示当前地区有t_user_0到3。）中的初始下标，这里就是0
                 int tableStartIdx = tableShardingNodes.get(0);
+                //终点下标，这里就是3
                 int tableEndIdx = tableShardingNodes.get(1);
+                //3-0+1=4，这里表示asia区一共有四个表
                 int tableRangeSize = tableEndIdx - tableStartIdx + 1;
+                //取到用户名信息
                 Comparable<?> usernameCompare = columnNameAndShardingValuesMap.get(usernameStr).stream().findFirst().get();
                 // 根据username来判定取到地区数据库范围中的哪一个
+                // hash(username)的值再对表范围取余，再加上起始下标（即表示从t_user_0-3这四个表中选择一个）
+                // 再除以单库中表的数量（这里是2），表示根据表下标来判定数据库下标（例如表下标为t_user_4,4/2=2，表示这个表在ds_2中）；
                 dbSuffix = String.valueOf((hashShardingValue(usernameCompare) % tableRangeSize + tableStartIdx) / this.tableShardingCount);
+                //固定写法，表示取到的数据库为 ds_x
                 result.add("ds_" + dbSuffix);
             }
         }
@@ -78,7 +85,7 @@ public class UserDataBaseComplexAlgorithm implements ComplexKeysShardingAlgorith
 
     /**
      * 根据地区名获取到shardingSphere配置文件中的各地表范围
-     * 如asia-database-sharding-count: 2 表示亚洲地区有两个数据库
+     * 如 asia-sharding-table-nodes: "{0..3}"表示亚洲地区的数据表范围是t_user_0-3
      */
     private List<Integer> getTableShardingNodes(String region) {
         String actualDataBaseShardingCountSuffix = region + SHARDING_TABLE_NODES_SUFFIX;
@@ -91,19 +98,27 @@ public class UserDataBaseComplexAlgorithm implements ComplexKeysShardingAlgorith
             Matcher matcher = pattern.matcher(s);
 
             if (matcher.matches()) {
+                //注意matcher.group要从1开始取，否则报错
                 int tableStartIdx = Integer.parseInt(matcher.group(1));
                 int tableEndIdx = Integer.parseInt(matcher.group(2));
                 return Lists.newArrayList(tableStartIdx, tableEndIdx);
             }
+        //这里要忽略异常，因为可能该地区名有误返回null，但是不妨碍下一步使用other地区
+        //如果直接抛异常的话程序将直接中断而不是使用other地区来保底
         }catch (Throwable ignored){}
         return null;
-        //throw new ShardingAlgorithmInitializationException(getType(), "region-sharding-nodes cannot be null");
     }
 
+    /**
+     * 根据props和查询字段来获取到单个数据库下分表数
+     */
     private int getTableShardingCount(Properties props) {
         return Optional.ofNullable(props.getProperty(TABLE_SHARDING_COUNT)).map(Integer::parseInt).orElseThrow(() -> new ShardingAlgorithmInitializationException(getType(), "table-sharding-count cannot be null"));
     }
 
+    /**
+     * 取hash值
+     */
     private long hashShardingValue(final Comparable<?> shardingValue) {
         return Math.abs((long) shardingValue.hashCode());
     }
--- src/main/java/com/lctking/shardingtest/algorithm/UserTableComplexAlgorithm.java
+++ src/main/java/com/lctking/shardingtest/algorithm/UserTableComplexAlgorithm.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Lists;
 import lombok.Getter;
 import org.apache.shardingsphere.sharding.api.sharding.complex.ComplexKeysShardingAlgorithm;
 import org.apache.shardingsphere.sharding.api.sharding.complex.ComplexKeysShardingValue;
+import org.apache.shardingsphere.sharding.exception.algorithm.sharding.ShardingAlgorithmClassImplementationException;
 import org.apache.shardingsphere.sharding.exception.algorithm.sharding.ShardingAlgorithmInitializationException;
 import cn.hutool.core.collection.CollUtil;
 
@@ -27,7 +28,9 @@ public class UserTableComplexAlgorithm implements ComplexKeysShardingAlgorithm {
     private static final String TABLE_SHARDING_COUNT = "table-sharding-count";
     @Override
     public Collection<String> doSharding(Collection availableTargetNames, ComplexKeysShardingValue shardingValues) {
+        //获取到分片涉及的字段，这里就是username和region与它们各自的值形成的键值对map
         Map<String, Collection<Comparable<?>>> columnNameAndShardingValuesMap = shardingValues.getColumnNameAndShardingValuesMap();
+        //固定写法，arraylist大小为可用目标名的大小
         Collection<String> result = new ArrayList<>(availableTargetNames.size());
         if(CollUtil.isNotEmpty(columnNameAndShardingValuesMap)){
             String usernameStr = "username";
@@ -35,17 +38,28 @@ public class UserTableComplexAlgorithm implements ComplexKeysShardingAlgorithm {
             Comparable<?> regionCompare = columnNameAndShardingValuesMap.get(regionStr).stream().findFirst().get();
             String actualRegion = regionCompare.toString();
             // 获取到表范围and该区域的表的起始下标
+            //根据region地区，取到分表节点信息（形如{0..3}，表示当前地区有t_user_0到3。）
+            //tableShardingNodes中存放的是[0,3]
             List<Integer> tableShardingNodes = getTableShardingNodes(actualRegion);
             if(CollUtil.isEmpty(tableShardingNodes)){
+                //如果根据当前region找不到数据表，则选择查询other地区的数据表（也意味着准备放入other地区对应的数据库表中
                 tableShardingNodes = getTableShardingNodes("other");
             }
+            if(CollUtil.isEmpty(tableShardingNodes)){
+                // 该地区和other地区的分表节点配置都找不到
+                throw new NullPointerException(String.format("The configuration of sharding-table-nodes in the %s region and other region cannot be found",actualRegion));
+            }
+            //取到分表节点信息（形如{0..3}，表示当前地区有t_user_0到3。）中的初始下标，这里就是0
             int tableStartIdx = tableShardingNodes.get(0);
+            //终点下标，这里就是3
             int tableEndIdx = tableShardingNodes.get(1);
+            //3-0+1=4，这里表示asia区一共有四个表
             int tableRangeSize = tableEndIdx - tableStartIdx + 1;
-
             // 获取到username
             Comparable<?> usernameCompare = columnNameAndShardingValuesMap.get(usernameStr).stream().findFirst().get();
+            // hash(username)的值再对表范围取余，再加上起始下标（即表示从t_user_0-3这四个表中选择一个）
             String tbSuffix = String.valueOf(hashShardingValue(usernameCompare) % tableRangeSize + tableStartIdx);
+            //固定写法，逻辑表名拼接_tbSuffix
             result.add(shardingValues.getLogicTableName()+"_"+tbSuffix);
         }
         return result;
@@ -68,9 +82,8 @@ public class UserTableComplexAlgorithm implements ComplexKeysShardingAlgorithm {
 
     /**
      * 根据地区名获取到shardingSphere配置文件中的各地表范围
-     * 如asia-database-sharding-count: 2 表示亚洲地区有两个数据库
+     * 如 asia-sharding-table-nodes: "{0..3}"表示亚洲地区的数据表范围是t_user_0-3
      */
-
     private List<Integer> getTableShardingNodes(String region) {
         String actualDataBaseShardingCountSuffix = region + SHARDING_TABLE_NODES_SUFFIX;
         String s = this.props.getProperty(actualDataBaseShardingCountSuffix);
@@ -87,6 +100,8 @@ public class UserTableComplexAlgorithm implements ComplexKeysShardingAlgorithm {
                 int tableEndIdx = Integer.parseInt(matcher.group(2));
                 return Lists.newArrayList(tableStartIdx, tableEndIdx);
             }
+        //这里要忽略异常，因为可能该地区名有误返回null，但是不妨碍下一步使用other地区
+        //如果直接抛异常的话程序将直接中断而不是使用other地区来保底
         }catch (Throwable ignored){}
         return null;
         //throw new ShardingAlgorithmInitializationException(getType(), "region-sharding-nodes cannot be null");
